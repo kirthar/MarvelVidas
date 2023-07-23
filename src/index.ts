959fa39fe3ff91911210ab3f2ace9b1cdae8da14
Eric Herrera Cano <ericheca11@gmail.com>
2023-07-23 23:01:13 +0200

    pendiente de arreglar icono web

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ console.log("Hola Mundo TS");
 const PORT  = 3001;
 import app from "./server";
 
-const favicon = require("serve-favicon")
+// const favicon = require("serve-favicon")
 
 app.listen(PORT , () => {
   console.log(`Example app listening on port ${PORT}`)
 })
-
+//app.use(favicon(__dirname + "/public/images/escudo_capi.jpg")); //
 
