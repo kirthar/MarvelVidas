7d7f964dcedd88c261ac786ec10201bb1480a562
Chris Fuertes <chris.fuertes@gmail.com>
2023-07-23 14:34:43 +0200

    Separar server e index

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,9 @@
 console.log("Hola Mundo TS");
+
+const PORT  = 3001;
+import app from "./server";
+
+app.listen(PORT , () => {
+  console.log(`Example app listening on port ${PORT}`)
+})
+
--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,9 @@
 import express from 'express'
 
 const app = express()
-const port = 3001
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Hello World! Se contar vidas')
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+export default app;
