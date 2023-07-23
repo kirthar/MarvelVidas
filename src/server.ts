dfa387aa404f769979020c1d9b70b13c710ae0e3
Eric Herrera Cano <ericheca11@gmail.com>
2023-07-23 16:05:03 +0200

    arreglar html

--- src/server.ts
+++ src/server.ts
@@ -4,12 +4,12 @@ const app = express()
 
 app.get('/', (request, response) => {
 
-  response.sendFile("E:/code/MarvelVidas/src/public/index.html");
+  response.sendFile( __dirname+ "/public/index.html");
 })
 
 app.get('/adios', (request, response) => {
 
-  response.sendFile("E:/code/MarvelVidas/src/public/adios.html");
+  response.sendFile( __dirname +"/public/adios.html");
 })
 
 export default app;
