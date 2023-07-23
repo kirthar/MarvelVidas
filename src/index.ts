bb6714f3ef14ecfba3112099cabf2602cf591fb7
Eric Herrera Cano <ericheca11@gmail.com>
2023-07-23 22:39:44 +0200

    nueva rama para el html

--- src/index.ts
+++ src/index.ts
@@ -3,7 +3,10 @@ console.log("Hola Mundo TS");
 const PORT  = 3001;
 import app from "./server";
 
+const favicon = require("serve-favicon")
+
 app.listen(PORT , () => {
   console.log(`Example app listening on port ${PORT}`)
 })
+
 
