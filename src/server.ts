caee3cbe4f23ebb38dee4f5941a4da1224039f6c
Chris Fuertes <chris.fuertes@gmail.com>
2023-07-23 14:45:18 +0200

    Añadir htmls de ejemplo

--- src/server.ts
+++ src/server.ts
@@ -2,8 +2,14 @@ import express from 'express'
 
 const app = express()
 
-app.get('/', (req, res) => {
-  res.send('Hello World! Se contar vidas')
+app.get('/', (request, response) => {
+
+  response.sendFile("E:/code/MarvelVidas/src/public/index.html");
+})
+
+app.get('/adios', (request, response) => {
+
+  response.sendFile("E:/code/MarvelVidas/src/public/adios.html");
 })
 
 export default app;
