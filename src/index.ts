2e0e6a89dead09041d998ef8570ae359813596e9
Eric Herrera Cano <ericheca11@gmail.com>
2023-07-28 18:03:00 +0200

    favicon arreglado (ni idea de como)

--- src/index.ts
+++ src/index.ts
@@ -4,10 +4,13 @@ const PORT  = 3001;
 import express from 'express'
 import app from "./server";
 
-// const favicon = require("serve-favicon")
+
+import favicon from "serve-favicon"
 
 app.listen(PORT , () => {
   console.log(`Example app listening on port ${PORT}`)
 })
-//app.use(favicon(__dirname + "/public/images/escudo_capi.jpg")); //
-app.use(express.static(__dirname+"/public"));
+app.use(favicon(__dirname + "/public/images/escudo_capi.jpg")); 
+
+
+//app.use((express.static(__dirname+"/public")));//
