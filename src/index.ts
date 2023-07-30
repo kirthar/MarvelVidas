9af01d0149ad70555bdc65a5cb0d476ed5cbd102
Eric Herrera Cano <ericheca11@gmail.com>
2023-07-30 23:21:42 +0200

    lector de vida villano funciona, falta revisarlo un poco mas

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,29 @@ console.log("Hola Mundo TS");
 const PORT  = 3001;
 import express from 'express'
 import app from "./server";
-
+import readNumberFromFile from './leer_vidas';
 
 import favicon from "serve-favicon"
+import { error } from 'console';
+
+const Filename:string="./Vida_Villano.txt"
+
 
 app.listen(PORT , () => {
   console.log(`Example app listening on port ${PORT}`)
 })
 app.use(favicon(__dirname + "/public/images/escudo_capi.jpg")); 
 
+app.use((express.static(__dirname+"/public")));//
 
-//app.use((express.static(__dirname+"/public")));//
+
+let HpVillain= readNumberFromFile(Filename)
+.then((HP)=>{
+  console.log("Ur Villain HP is: " + HP)
+
+})
+.catch((error)=>{
+
+  console.log("something has suceded reading the villain HP: "+ error)
+
+})
---
+++ src/leer_vidas.ts
@@ -0,0 +1,43 @@
+import * as path from "fs"
+
+
+
+
+
+
+function readNumberFromFile(filePath: string): Promise<number> {
+    return new Promise((resolve, reject) => {
+      path.readFile(filePath, 'utf8', (err, data) => {
+        if (err) {
+          reject(err);
+        } else {
+          const numberValue = parseInt(data.trim(), 10);
+          if (isNaN(numberValue)) {
+            reject(new Error('The content of the file is not a valid number.'));
+          } else {
+            resolve(numberValue);
+          }
+        }
+      });
+    });
+}
+
+
+
+
+
+
+
+export default readNumberFromFile
+
+
+
+
+
+
+
+
+
+
+
+
